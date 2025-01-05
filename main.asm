27ec2b1ad57a9f03f2185549e5e81531a8f99d10
Martin Grap <rmsk2@gmx.de>
2025-01-05 18:49:21 +0100

    Let user select program to start via cursor keys

--- main.asm
+++ main.asm
@@ -16,15 +16,16 @@ jmp main
 .include "khelp.asm"
 .include "key_repeat.asm"
 
-TXT_MSG   .text "**** Select program to start from cartridge ****", $0d, $0d, $0d
-TXT_SNAKE .text "1. Snake", $0d, $0d
-TXT_2048  .text "2. 2048", $0d, $0d
-TXT_15    .text "3. 15 Puzzle", $0d, $0d
-TXT_EXIT  .text "4. Exit to BASIC"
-TXT_FCART .text "f. fcart", $0d, $0d
+TXT_AST         .text "****                                        ****", $0d
+TXT_MSG         .text "**** Select program to start from cartridge ****", $0d
+TXT_STARS       .text "************************************************", $0d
+TXT_SELECT_INFO .text "Start entry by typing the corresponding character or select", $0d
+TXT_SEL_INFO2   .text "entry with cursor keys and press return to start it"
 
-COL = $12 
-REV_COL = $21
+COL = $10 
+REV_COL = $01
+CRSR_UP = 16
+CRSR_DOWN = 14
 
 toRev .macro
     pha
@@ -41,6 +42,7 @@ noRev .macro
 .endmacro
 
 FCART_PRESENT .byte 0
+CURRENT_ENTRY .byte 0
 
 main
     jsr setup.mmu
@@ -49,21 +51,17 @@ main
     jsr txtio.init80x60
     ;jsr txtio.cursorOn
 
-    lda #$12
+    lda #$10
     sta CURSOR_STATE.col 
-    jsr txtio.clear
+    jsr txtio.clear    
 
-    #printString TXT_MSG, len(TXT_MSG)
-    #printString TXT_SNAKE, len(TXT_SNAKE)
-    #printString TXT_2048, len(TXT_2048)
-    #printString TXT_15, len(TXT_15)
+    stz CURRENT_ENTRY
     stz FCART_PRESENT
     jsr checkFcart
-    bcc _exitBasic
+    bcc _notPresent
     inc FCART_PRESENT
-    #printString TXT_FCART, len(TXT_FCART)
-_exitBasic
-    #printString TXT_EXIT, len(TXT_EXIT)
+_notPresent
+    jsr printAvailable
 
 _restart
     jsr keyrepeat.init
@@ -76,57 +74,234 @@ _restart
     rts
 
 
+Entry_t .struct  cmd, addr, txt, len
+    command  .byte \cmd
+    ref      .word \addr
+    text     .word \txt
+    textLen  .byte \len
+    reserved .fill 2
+.endstruct
+
+NUM_CHARS_ACCEPTED = 3
+
+TXT_SNAKE .text "Snake: A simple clone of the game snake", $0d, $0d
+TXT_2048  .text "2048: The well known block shifting game", $0d, $0d
+TXT_15    .text "15 Puzzle: The original block shifting game", $0d, $0d
+TXT_FCART .text "f. fcart: Program to write data to the flash cartridge", $0d, $0d
+TXT_EXIT  .text "x. Exit to BASIC"
+
+REF_TABLE
+A .dstruct Entry_t, '1', SNAKE, TXT_SNAKE, len(TXT_SNAKE)
+B .dstruct Entry_t, '2', F2048, TXT_2048, len(TXT_2048)
+C .dstruct Entry_t, '3', F15, TXT_15, len(TXT_15)
+
+
 processKeyEvent
     sta ASCII_TEMP
+    cmp #'x'
+    bne _checkFcart
+    clc
+    rts
+_checkFcart
+    jsr checkCallFcart
+    bcs _checkCursor
+    bra _goOn
+_checkCursor
+    lda ASCII_TEMP
+    cmp #CRSR_DOWN
+    bne _checkDown
+    inc CURRENT_ENTRY
+    clc
+    lda #NUM_CHARS_ACCEPTED
+    adc FCART_PRESENT
+    ina
+    cmp CURRENT_ENTRY
+    bne _redraw
+    stz CURRENT_ENTRY
+_redraw
+    jsr printAvailable
+    bra _goOn
+_checkDown
+    cmp #CRSR_UP
+    bne _checkStart
+    dec CURRENT_ENTRY
+    bpl _redraw2
+    clc
+    lda #NUM_CHARS_ACCEPTED
+    adc FCART_PRESENT
+    sta CURRENT_ENTRY
+_redraw2
+    jsr printAvailable
+    bra _goOn
+_checkStart
+    cmp #CARRIAGE_RETURN
+    bne _checkCallProgram
+    clc
+    lda #NUM_CHARS_ACCEPTED
+    adc FCART_PRESENT
+    cmp CURRENT_ENTRY
+    bne _notExit
+    clc 
+    rts
+_notExit
     lda FCART_PRESENT
-    beq _checkNumeric
+    beq _runEntry
+    lda #NUM_CHARS_ACCEPTED
+    cmp CURRENT_ENTRY
+    bne _runEntry
+    jsr callFcart
+    bra _goOn
+_runEntry
+    jsr runEntry
+    bra _goOn
+_checkCallProgram
+    jsr checkProgram
+_goOn
+    sec
+    rts
+    
+
+
+checkCallFcart
+    lda FCART_PRESENT
+    beq endNotFound
     lda ASCII_TEMP
     cmp #'f'
-    bne _checkNumeric
+    bne endNotFound
+callFcart
     #load16BitImmediate FCCART, kernel.args.buf
     jsr kernel.RunNamed
     #load16BitImmediate FCART, kernel.args.buf
     jsr kernel.RunNamed
-    bra _goOn
-_checkNumeric
-    lda ASCII_TEMP
-    cmp #'1'
-    bcc _goOn
-    cmp #'5'
-    bcs _goOn
-
-    cmp #'1'
-    bne _test2
-    #load16BitImmediate SNAKE, kernel.args.buf
-    jsr kernel.RunNamed
-    bra _goOn
-_test2
-    cmp #'2'
-    bne _test3
-    #load16BitImmediate F2048, kernel.args.buf
-    jsr kernel.RunNamed
-    bra _goOn
-_test3
-    cmp #'3'
-    bne _exit
-    #load16BitImmediate F15, kernel.args.buf
-    jsr kernel.RunNamed
-    bra _goOn
-; _test4
-;     cmp #'4'
-;     bne _exit
-;     #load16BitImmediate FCCART, kernel.args.buf
-;     jsr kernel.RunNamed
-;     #load16BitImmediate FCART, kernel.args.buf
-;     jsr kernel.RunNamed
-;     bra _goOn
-_exit
     clc
     rts
-_goOn
+endNotFound
     sec
     rts
 
+
+checkProgram   
+    ldy #0
+    ldx #0
+_loopAllowed
+    cpy #NUM_CHARS_ACCEPTED
+    beq _goOn
+    tya
+    asl
+    asl
+    asl
+    tax
+    lda ASCII_TEMP
+    cmp REF_TABLE, x
+    beq _start
+    iny
+    bra _loopAllowed
+_start
+    inx
+    lda REF_TABLE, x
+    sta kernel.args.buf
+    inx    
+    lda REF_TABLE, x
+    sta kernel.args.buf + 1
+    jsr kernel.RunNamed
+_goOn
+    rts
+
+
+runEntry
+    lda CURRENT_ENTRY
+    asl
+    asl
+    asl
+_special
+    tay
+    iny
+    lda REF_TABLE, y
+    sta kernel.args.buf
+    iny
+    lda REF_TABLE, y
+    sta kernel.args.buf + 1
+    jsr kernel.RunNamed
+    rts
+
+
+STRUCT_INDEX .byte 0
+printAvailable
+    jsr txtio.home
+    #printString TXT_STARS, len(TXT_STARS)
+    #printString TXT_AST, len(TXT_AST)
+    #printString TXT_MSG, len(TXT_MSG)
+    #printString TXT_AST, len(TXT_AST)
+    #printString TXT_STARS, len(TXT_STARS)
+    jsr txtio.newLine
+    jsr txtio.newLine
+
+    ldy #0
+_loopAllowed
+    cpy #NUM_CHARS_ACCEPTED
+    beq _done
+    cpy CURRENT_ENTRY
+    bne _noHighlight
+    #toRev
+_noHighlight    
+    phy
+    tya
+    asl
+    asl
+    asl
+    sta STRUCT_INDEX
+    clc
+    adc #Entry_t.command
+    tax
+    lda REF_TABLE, x
+    jsr txtio.charOut
+    lda #'.'
+    jsr txtio.charOut
+    lda #' '
+    jsr txtio.charOut
+    lda STRUCT_INDEX
+    clc
+    adc #Entry_t.text
+    tax
+    lda REF_TABLE, x
+    sta TXT_PTR3
+    inx
+    lda REF_TABLE, x
+    sta TXT_PTR3 + 1
+    inx
+    lda REF_TABLE, x
+    jsr txtio.printStr
+    #noRev
+    ply
+    iny
+    bra _loopAllowed
+_done
+    lda FCART_PRESENT
+    beq _noFcart
+    clc
+    lda #NUM_CHARS_ACCEPTED
+    cmp CURRENT_ENTRY
+    bne _l1
+    #toRev
+_l1
+    #printString TXT_FCART, len(TXT_FCART)
+    #noRev
+_noFcart
+    clc
+    lda #NUM_CHARS_ACCEPTED
+    adc FCART_PRESENT
+    cmp CURRENT_ENTRY
+    bne _l2
+    #toRev
+_l2
+    #printString TXT_EXIT, len(TXT_EXIT)
+    #noRev
+    jsr txtio.newLine
+    jsr txtio.newLine
+    jsr txtio.newLine
+    #printString TXT_SELECT_INFO, len(TXT_SELECT_INFO)
+    #printString TXT_SEL_INFO2, len(TXT_SEL_INFO2)
+    rts
 
 ; carry is set if strings are equal. String 1 in MEM_PTR1, the other has to
 ; be in MEM_PTR2.
@@ -166,12 +341,12 @@ checkFcart
     cmp #$56
     bne _notFound
 
-    #load16BitImmediate FCART, MEM_PTR1
+    #load16BitImmediate FCCART, MEM_PTR1
     #load16BitImmediate $A00A, MEM_PTR2
     jsr strCmp
     bcs _restoreMMU
 
-    #load16BitImmediate FCCART, MEM_PTR1
+    #load16BitImmediate FCART, MEM_PTR1
     #load16BitImmediate $A00A, MEM_PTR2
     jsr strCmp
     bcs _restoreMMU
